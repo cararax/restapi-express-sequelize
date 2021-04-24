2be57722d8b5b3b9c7ddae78bdd72652c08b94a8
Pedro Carara <cararax@gmail.com>
2021-04-24 09:32:50 -0300

    adicionando controller e rotas para student

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 const classroomController = require('../controllers').classroom;
 
+const studentController = require('../controllers').student;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -12,6 +14,12 @@ router.get('/api/classroom', classroomController.list);
 router.get('/api/classroom/:id', classroomController.getById);
 router.post('/api/classroom', classroomController.add);
 router.put('/api/classroom/:id', classroomController.update);
-router.delete('/api/classroom/:id', classroomController.delete);
+router.delete('/api/classroom/:id', classroomController.delete)
+
+router.get('/api/student', studentController.list);
+router.get('/api/student/:id', studentController.getById);
+router.post('/api/student', studentController.add);
+router.put('/api/student/:id', studentController.update);
+router.delete('/api/student/:id', studentController.delete);
 
 module.exports = router;
