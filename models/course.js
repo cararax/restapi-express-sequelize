a8bafbf870b7e743c88ae26f283cf3b47b86cf42
Pedro Carara <cararax@gmail.com>
2021-04-24 09:17:54 -0300

    adicionei o relacionamento entre os modelos e fiz o migration

--- models/course.js
+++ models/course.js
@@ -4,13 +4,16 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Course extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Course.belongsToMany(models.Student, {
+        through: 'StudentCourse',
+        as: 'students',
+        foreignKey: 'course_id'
+      });
+      Course.belongsTo(models.Lecturer, {
+        foreignKey: 'lecturer_id',
+        as: 'lecturer'
+      });
     }
   };
   Course.init({
--- models/student.js
+++ models/student.js
@@ -4,13 +4,16 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Student extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Student.belongsTo(models.Classroom, {
+        foreignKey: 'classroom_id',
+        as: 'classroom'
+      });
+      Student.belongsToMany(models.Course, {
+        through: 'StudentCourse',
+        as: 'courses',
+        foreignKey: 'student_id'
+      });
     }
   };
   Student.init({
